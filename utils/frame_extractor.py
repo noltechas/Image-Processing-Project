fc3f79064bd78103907b9d19d3b8d20aea9a0726
Chas <chasnolte@unomaha.edu>
2023-10-03 19:26:44 -0500

    Added frame_extractor

--- utils/frame_extractor.py
+++ utils/frame_extractor.py
@@ -3,42 +3,44 @@ import os
 
 from image_processing import apply_adaptive_thresholding, apply_color_clustering
 
-def extract_frames_from_video(video_path, original_folder, thresholded_folder, clustered_folder):
-    # Open the video
-    cap = cv2.VideoCapture(video_path)
+video_path = 'breaststroke.mp4'
+original_folder = '../data/original_frames'
+thresholded_folder = '../data/thresholded_frames'
+clustered_folder = '../data/clustered_frames'
 
-    if not cap.isOpened():
-        print(f"Error: Unable to open video file at {video_path}")
-        return
+# Create the necessary folders
+os.makedirs(original_folder, exist_ok=True)
+os.makedirs(thresholded_folder, exist_ok=True)
+os.makedirs(clustered_folder, exist_ok=True)
 
-    frame_count = 0
+# Open the video
+cap = cv2.VideoCapture(video_path)
+
+# Check if video opened successfully
+if not cap.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
+frame_number = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Save the original frame
+    original_filename = os.path.join(original_folder, f'frame_{frame_number}.jpg')
+    cv2.imwrite(original_filename, frame)
     
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        # Save the original frame
-        original_filename = os.path.join(original_folder, f'frame_{frame_count}.jpg')
-        cv2.imwrite(original_filename, frame)
-        
-        # Save the thresholded frame
-        thresholded_frame = apply_adaptive_thresholding(frame)
-        thresholded_filename = os.path.join(thresholded_folder, f'frame_{frame_count}.jpg')
-        cv2.imwrite(thresholded_filename, thresholded_frame)
-        
-        # Save the clustered frame
-        clustered_frame = apply_color_clustering(frame)
-        clustered_filename = os.path.join(clustered_folder, f'frame_{frame_count}.jpg')
-        cv2.imwrite(clustered_filename, clustered_frame)
-        
-        frame_count += 1
+    # Save the thresholded frame
+    thresholded_frame = apply_adaptive_thresholding(frame)
+    thresholded_filename = os.path.join(thresholded_folder, f'frame_{frame_number}.jpg')
+    cv2.imwrite(thresholded_filename, thresholded_frame)
     
-    cap.release()
+    # Save the clustered frame
+    clustered_frame = apply_color_clustering(frame)
+    clustered_filename = os.path.join(clustered_folder, f'frame_{frame_number}.jpg')
+    cv2.imwrite(clustered_filename, clustered_frame)
+    
+    frame_number += 1
 
-if __name__ == "__main__":
-    video_path = '../test_images/breaststroke.mp4'  # Hardcoded path to the video
-    original_folder = '../data/original_frames'  # Folder for original frames
-    thresholded_folder = '../data/thresholded_frames'  # Folder for thresholded frames
-    clustered_folder = '../data/clustered_frames'  # Folder for clustered frames
-    extract_frames_from_video(video_path, original_folder, thresholded_folder, clustered_folder)
+cap.release()
