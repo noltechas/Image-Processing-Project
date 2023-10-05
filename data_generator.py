a8d7dab11b47518d2096a0601f11a71cc3efd240
BuildTools <unconfigured@null.spigotmc.org>
2023-10-04 19:42:00 -0500

    Got new model working

---
+++ data_generator.py
@@ -0,0 +1,144 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import json
+import tensorflow as tf
+
+class DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, image_dir, csv_path, batch_size=32, shuffle=True):
+        self.image_dir = image_dir
+        self.keypoints_data = self.load_keypoints_from_csv(csv_path)
+        self.filenames = list(self.keypoints_data.keys())
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        if self.shuffle:
+            np.random.shuffle(self.filenames)
+
+    def load_keypoints_from_csv(self, csv_path):
+        df = pd.read_csv(csv_path)
+        keypoints_data = {}
+        for _, row in df.iterrows():
+            filename = row['filename']
+
+            # Extracting cx and cy from the string
+            point_data_str = row['region_shape_attributes']
+            point_data = json.loads(point_data_str.replace('""', '"').replace('undefined', '"undefined"'))
+
+            # Check if 'cx' and 'cy' keys are present in point_data
+            if 'cx' not in point_data or 'cy' not in point_data:
+                continue
+
+            x = point_data['cx']
+            y = point_data['cy']
+
+            joint_str = row['region_attributes'].replace('undefined', 'Lhand')  # Handle 'undefined' joint names
+
+            # Check if joint_str is a valid JSON string
+            try:
+                joint_data = json.loads(joint_str.replace('""', '"'))
+            except json.JSONDecodeError:
+                continue
+
+            joint_name = joint_data.get('joint', 'Lhand')
+
+            if filename not in keypoints_data:
+                keypoints_data[filename] = {'x': [], 'y': [], 'joint': []}
+            keypoints_data[filename]['x'].append(x)
+            keypoints_data[filename]['y'].append(y)
+            keypoints_data[filename]['joint'].append(joint_name)
+        return keypoints_data
+
+    def generate_heatmap(self, image_shape, keypoints, sigma=3):
+        num_keypoints = 14  # Set this to the expected number of keypoints
+        heatmap = np.zeros((*image_shape[:2], num_keypoints), dtype=np.float32)
+        for idx, joint in enumerate(sorted(list(set(keypoints['joint'])))):  # Sort the keypoints for consistency
+            temp_heatmap = np.zeros(image_shape[:2], dtype=np.float32)  # Temporary 2D array
+            for x, y, j in zip(keypoints['x'], keypoints['y'], keypoints['joint']):
+                if j == joint:
+                    temp_heatmap = cv2.circle(temp_heatmap, (x, y), sigma, (1,), -1)
+            heatmap[:, :, idx] = temp_heatmap
+            heatmap[:, :, idx] /= np.max(heatmap[:, :, idx])  # Normalize to [0, 1]
+        return heatmap
+
+
+    def generate_vectormap(self, image_shape, keypoints):
+        num_keypoints = 14
+        vectormap = np.zeros((*image_shape[:2], 2*num_keypoints), dtype=np.float32)
+
+        connections = [('Lshoulder', 'Lelbow'), ('Lhand','Lelbow'), ('Rshoulder', 'Relbow'), ('Rhand', 'Relbow'), ('Rshoulder', 'Lshoulder'),
+                       ('Lshoulder', 'Lhip'), ('Rshoulder', 'Rhip'), ('Rhip', 'Lhip'), ('Rhip', 'Rknee'), ('Lhip', 'Lknee'), ('Rknee', 'Rfoot'),
+                       ('Rknee', 'Rfoot'), ('Rshoulder', 'head'), ('Lshoulder', 'head'),]  # Add more connections as needed
+
+
+        for start, end in connections:
+            if start in keypoints['joint'] and end in keypoints['joint']:
+                start_idx = keypoints['joint'].index(start)
+                end_idx = keypoints['joint'].index(end)
+                start_point = (keypoints['x'][start_idx], keypoints['y'][start_idx])
+                end_point = (keypoints['x'][end_idx], keypoints['y'][end_idx])
+                direction = np.array(end_point) - np.array(start_point)
+                unit_vector = direction / np.linalg.norm(direction)
+
+                # Determine the channels for the vectormap
+                start_channel = sorted(list(set(keypoints['joint']))).index(start) * 2
+                end_channel = sorted(list(set(keypoints['joint']))).index(end) * 2
+
+                # Create temporary 2D arrays for the x and y components
+                temp_map_x = np.zeros(image_shape[:2], dtype=np.float32)
+                temp_map_y = np.zeros(image_shape[:2], dtype=np.float32)
+
+                # Draw the lines on the temporary 2D arrays
+                cv2.line(temp_map_x, start_point, end_point, unit_vector[0], 1)
+                cv2.line(temp_map_y, start_point, end_point, unit_vector[1], 1)
+
+                # Assign the temporary 2D arrays to the appropriate channels of the vectormap
+                vectormap[:, :, start_channel] = temp_map_x
+                vectormap[:, :, start_channel+1] = temp_map_y
+
+        vectormap = cv2.resize(vectormap, (image_shape[1], image_shape[0]))  # Resize to match the input shape
+        return vectormap
+
+
+
+    def display_image_with_keypoints(self, image, keypoints):
+        """Displays the image with keypoints overlaid."""
+        img_copy = image.copy()
+        for x, y in zip(keypoints['x'], keypoints['y']):
+            cv2.circle(img_copy, (x, y), 3, (0, 255, 0), -1)  # Green color for keypoints
+        cv2.imshow('Image with Keypoints', img_copy)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def __len__(self):
+        return int(np.ceil(len(self.filenames) / self.batch_size))
+
+    def __getitem__(self, index):
+        batch_filenames = self.filenames[index*self.batch_size:(index+1)*self.batch_size]
+        batch_images = []
+        batch_heatmaps = []
+        batch_vectormaps = []
+
+        for filename in batch_filenames:
+            image_path = os.path.join(self.image_dir, filename)
+            image = cv2.imread(image_path)
+            keypoints = self.keypoints_data[filename]
+            heatmap = self.generate_heatmap(image.shape, keypoints)
+            vectormap = self.generate_vectormap(image.shape, keypoints)
+
+            # Resize the image, heatmap, and vectormap to a consistent size if necessary
+            target_shape = (640, 480)  # Adjust this to your desired shape
+            image = cv2.resize(image, target_shape)
+            heatmap = cv2.resize(heatmap, target_shape)
+            vectormap = cv2.resize(vectormap, target_shape)
+
+            batch_images.append(image)
+            batch_heatmaps.append(heatmap)
+            batch_vectormaps.append(vectormap)
+
+        return np.array(batch_images), [np.array(batch_heatmaps), np.array(batch_vectormaps)]
+
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            np.random.shuffle(self.filenames)
--- model.py
+++ model.py
@@ -1,94 +1,64 @@
-import cv2
-import numpy as np
-from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Flatten, Dense
-from utils.data_preprocessing import load_keypoints_from_csv
-import os
-os.environ['LOKY_MAX_CPU_COUNT'] = '4'
+import tensorflow as tf
+from data_generator import DataGenerator
+import keras.layers
 
-def apply_adaptive_thresholding(image):
-    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresholded = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    return thresholded
 
-def generate_vector_map(image_shape, keypoints):
-    h, w = image_shape[0], image_shape[1]
-    vector_map = np.zeros((h, w, 2))
+def create_model(input_shape, num_keypoints):
+    inputs = tf.keras.layers.Input(shape=input_shape)
 
-    for i in range(h):
-        for j in range(w):
-            min_distance = float('inf')
-            closest_keypoint = None
+    # Block 1
+    x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
+    x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
-            for kp_x, kp_y in zip(keypoints['x'], keypoints['y']):
-                distance = np.sqrt((kp_x - j)**2 + (kp_y - i)**2)
-                if distance < min_distance:
-                    min_distance = distance
-                    closest_keypoint = (kp_x, kp_y)
+    # Block 2
+    x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
-            vector_map[i, j, 0] = closest_keypoint[0] - j
-            vector_map[i, j, 1] = closest_keypoint[1] - i
+    # Block 3
+    x = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
-    return vector_map
+    # Block 4
+    x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
-def normalize_vector_map(vector_map):
-    # Shift all values to be positive
-    shifted_vector_map = vector_map - np.min(vector_map)
-    # Normalize to [0, 1]
-    normalized_vector_map = shifted_vector_map / np.max(shifted_vector_map)
-    return normalized_vector_map
+    # Block 5
+    x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
 
-# Load keypoints
-csv_path = 'data/mappings/breaststroke.csv'
-keypoints_data = load_keypoints_from_csv(csv_path)
+    # Adjust the number of filters in the last Conv2D layer to match the number of keypoints
+    heatmap_output = keras.layers.Conv2D(num_keypoints, (1, 1), activation='sigmoid', name='heatmap_output')(x)
+    heatmap_output = keras.layers.UpSampling2D(size=(16, 16))(heatmap_output)  # Upsample to match the input shape
 
-# Load images and prepare data
-images = []
-vector_maps = []
+    vectormap_output = keras.layers.Conv2D(2*num_keypoints, (1, 1), activation='tanh', name='vectormap_output')(x)
+    vectormap_output = keras.layers.UpSampling2D(size=(16, 16))(vectormap_output)  # Upsample to match the input shape
 
-for filename, data in keypoints_data.items():
-    # Load original image and resize it
-    img_original = cv2.imread(f"data/original_frames/{filename}")
-    img_resized = cv2.resize(img_original, (240, 320))  # Half the original size
+    model = keras.Model(inputs=inputs, outputs=[heatmap_output, vectormap_output])
+    return model
 
-    # Generate thresholded version
-    img_thresholded = apply_adaptive_thresholding(img_resized)
 
-    # Add an additional dimension to the thresholded image
-    img_thresholded = np.expand_dims(img_thresholded, axis=-1) / 255.0
-    images.append(img_thresholded)
+if __name__ == "__main__":
+    # Parameters
+    image_dir = 'data/original_frames'
+    csv_path = 'data/mappings/breaststroke.csv'
+    input_shape = (None, None, 3)  # Height, Width, Channels
 
-    # Generate and normalize vector map
-    vm = generate_vector_map(img_resized.shape[:2], {'x': data['x'], 'y': data['y']})
-    normalized_vm = normalize_vector_map(vm)
-    vector_maps.append(normalized_vm)
+    # Assuming there are 14 keypoints (based on the connections you provided)
+    num_keypoints = 14
 
-# Separate images and keypoints
-X = np.array(images)
-y = np.array(vector_maps)
+    # Create model
+    model = create_model(input_shape, num_keypoints)
+    model.summary()
 
-# Split data
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+    # Compile the model
+    model.compile(optimizer='adam', loss=['mean_squared_error', 'mean_squared_error'], metrics=['accuracy'])
 
-# Define model for vector maps
-model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(320, 240, 1)),
-    MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu', padding='same'),
-    MaxPooling2D(2, 2),
-    Conv2D(128, (3, 3), activation='relu', padding='same'),
-    UpSampling2D((2, 2)),  # Upsample the feature maps
-    Conv2D(64, (3, 3), activation='relu', padding='same'),
-    UpSampling2D((2, 2)),  # Upsample the feature maps
-    Conv2D(2, (1, 1), activation='sigmoid')  # Output 2 channels for x and y vector maps
-])
+    # Initialize data generator
+    data_gen = DataGenerator(image_dir, csv_path)
 
-model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=2500)
-
-# Save the model
-model_path = "my_model.h5"
-model.save(model_path)
-
-print(f"Model saved to {model_path}")
+    # Train the model
+    model.fit(data_gen, epochs=10)
